d1d35e38628ecb36a4a4e8806c3515972eec45db
lucas marques de almeida <luq.marqs@gmail.com>
2022-04-24 15:27:30 -0300

    implementando a impressão do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,6 +1,9 @@
 ﻿using tabuleiro;
+using xadrez;
 using xadrez_console;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
 
 Tela.imprimirTabuleiro(tab);
