b37c23a76d357452dd4715f181ca4a79224671f6
Lucas Marques <66765396+luqmarqs@users.noreply.github.com>
2022-04-25 11:39:21 -0300

    implementação dos movimentos possíveis do rei e da torre, testes iniciais de movimento das peças, mudanças no método de impressão do tabuleiro

--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -14,5 +14,70 @@ namespace xadrez
         {
             return "T";
         }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while(Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != this.Cor)
+                    break;
+                
+                pos.Linha--;
+            }
+
+            //abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != this.Cor)
+                    break;
+
+                pos.Linha++;
+            }
+
+            //direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != this.Cor)
+                    break;
+
+                pos.Coluna++;
+            }
+
+            //esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != this.Cor)
+                    break;
+
+                pos.Coluna--;
+            }
+
+            return mat;
+
+
+        }
     }
 }
